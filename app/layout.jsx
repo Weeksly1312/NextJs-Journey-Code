c8e1c69154c59d5b84777c7b094954555db8614b
Weeksly1312 <S.elhallaoui01@gmail.com>
2024-01-03 21:21:34 +0100

    modify the layout

--- app/layout.jsx
+++ app/layout.jsx
@@ -1,14 +1,20 @@
-import '@styles/globals.css';
+import "@styles/globals.css";
 
 export const metadata = {
-  title: 'Nextjs Journey Code',
-  description: 'My Journey to Learn Next.js',
-}
+  title: "Nextjs Journey Code",
+  description: "My Journey to Learn Next.js",
+};
 
 export default function RootLayout({ children }) {
- return (
+  return (
     <html lang="en">
-      <body>{children}</body>
+      <body>
+        <div className="main">
+          <div className="gradient" />
+        </div>
+
+        <main className="app">{children}</main>
+      </body>
     </html>
-  )
+  );
 }
